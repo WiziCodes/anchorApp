c086e5df2439476c57db3f7aad90bfa85b748985
WiziCodes <wisdomajibade06@gmail.com>
2023-08-13 16:06:31 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,155 @@
+AOS.init()
+
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slideShow");
+    let spotlight = document.getElementsByClassName("spotlight");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < spotlight.length; i++) {
+        spotlight[i].className = spotlight[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    // spotlight[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+const toggle = document.querySelector(".toggle")
+const sideBar = document.querySelector(".sideBar")
+const navItems = document.querySelectorAll('.navItems li')
+
+toggle.addEventListener("click", expand)
+
+
+function expand() {
+    var state = sideBar.style.width;
+    if (state == "60px") {
+        sideBar.style.width = "130px";
+        navItems.forEach((i) => {
+            i.style.justifyContent = "space-between";
+            i.style.flexDirection = "row";
+            i.style.fontSize = "14px";
+        })
+
+    }
+    else {
+        sideBar.style.width = "60px";
+        navItems.forEach((i) => {
+            i.style.justifyContent = "center";
+            i.style.flexDirection = "column";
+            i.style.fontSize = "10px";
+        })
+    }
+}
+// const button = document.getElementById("button")
+// button.addEventListener("click", show)
+
+// function show() {
+//     document.querySelector(".phonesideBar").style.display = "block";
+// }
+
+function hide() {
+    document.querySelector(".phonesideBar").style.display = "none";
+}
+// function showSideBar() {
+//     document.querySelector(".phonesideBar").style.display = "flex";
+// }
+
+const input = document.getElementById('searchBar')
+
+if (window.innerWidth < 430) {
+    input.addEventListener("focus", hide)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var i = 0;
+// var imgs = []
+// var img2 = []
+// var titles = []
+// var titles2 = []
+// var time = 10000;
+// var url1 = "/LandingPage/8949375.jpg"//mario
+// var url2 = "/LandingPage/spidermanBack.jpg"//spiderman
+// var url3 = "/LandingPage/8482387.jpg"//galaxy
+
+
+
+// imgs[0] = `url(${url1})`;//mario
+// img2[0] = `/LandingPage/9048118.jpg`
+
+// imgs[1] = `url(${url2})`;//spiderman
+// img2[1] = `/LandingPage/spidermanBack.jpg`
+
+// imgs[2] = `url(${url3})`;//galaxy
+// img2[2] = `/LandingPage/9112682.jpg`
+
+
+// titles[0] = '#1 SPOTLIGHT'
+// titles[1] = '#2 SPOTLIGHT'
+// titles[2] = ' #3 SPOTLIGHT'
+// titles2[0] = 'The SUPER MARIO BROS.movie'
+// titles2[1] = 'Spider Man: Across the Spider Verse'
+// titles2[2] = 'Guardians of the Galaxy. volume3'
+
+
+// function slider() {
+//     // document.getElementById("title").innerText = titles[p];
+//     document.getElementById("slideShow").style.backgroundImage = imgs[i];
+//     document.querySelector(".spotlight").innerHTML = titles[i];
+//     document.querySelector(".title").innerHTML = titles2[i];
+
+//     document.img.src = img2[i]
+//     if (i < img2.length - 1) {
+//         i++;
+//     } else {
+//         i = 0;
+//     }
+//     setTimeout('slider()', time)
+// }
+// window.onload = slider;
+// // console.log(imgs.length)
+
+
+
---
+++ search.js
@@ -0,0 +1,198 @@
+function searchMovies() {
+    let input = document.getElementById('searchBar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName("sm");
+    let counter = 0;
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+            counter++;
+            // document.querySelector(".searchStatus").style.display = "block";
+        }
+        else {
+            x[i].style.display = "list-item";
+            // document.querySelector(".searchStatus").style.display = "none";
+        }
+        // console.log(x[i].getComputedStyle())
+    }
+    if(counter>=x.length){
+            document.querySelector(".searchStatus").style.display = "block";
+        }
+        else{
+            document.querySelector(".searchStatus").style.display = "none";
+  
+        }
+    if (input.length > 0) {
+        document.getElementById("Trending").style.display = "none"
+        document.getElementById("Discover").style.display = "none"
+        document.getElementById("disp").style.display = "none";
+        document.querySelectorAll(".movieClassCont").forEach((i) => {
+            i.style.display = "none"
+        })
+        document.querySelector(".phonesideBar").style.display = "none";
+        document.querySelector(".backCont").style.display = "block";
+
+    } else {
+        document.getElementById("Trending").style.display = "block"
+        document.getElementById("Discover").style.display = "block"
+        document.getElementById("disp").style.display = "block";
+        document.querySelector(".phonesideBar").style.display = "";
+        document.querySelectorAll(".movieClassCont").forEach((i) => {
+            i.style.display = "flex"
+        })
+        document.querySelector(".backCont").style.display = "none";
+
+    }
+    if (input.length > 0 && window.innerWidth <= 380) {
+        document.querySelectorAll("ol").forEach((i) => {
+            i.style.display = "grid"
+            i.style.gridTemplateColumns = "1fr"
+            console.log(window.innerWidth)
+        })
+    } else {
+        document.querySelectorAll("ol").forEach((i) => {
+            i.style.display = "";
+        })
+    }
+}
+
+
+
+
+//recents
+const recentMovies = document.getElementById("recents");
+
+const fetchRecents = async () => {
+    const response = await fetch("https://mocki.io/v1/46598bec-8c48-45a2-9bf2-9016742d71ba")
+    const movies = response.json();
+    return movies;
+};
+
+const createRecents = async () => {
+    const movies = await fetchRecents()
+    if (movies?.length) {
+        movies?.forEach((movie) => {
+            const li = document.createElement("li");
+            li.classList.add("sm")
+            li.innerHTML = `
+                <div class="movies">
+                    <img src=${movie?.movieImg}>
+                    <div class="movieInfo">
+                        <div class="sourceCont">
+                            <div class="sourceText">
+                                <p>Source:</p>
+                            </div>
+                            <div class="source">
+                                <span>${movie?.movieSourceText}</span>
+                                <img src="Icons/images (1).png">
+                            </div>
+                        </div>
+                        <div class="movieTitleCont">
+                            <h4>${movie?.movieTitle}</h4>
+                        </div>
+                        <div class="movieDesc">
+                            <p>${movie?.description}</p>
+                        </div>
+                        <div class="viewLinkCont">
+                            <a href=${movie?.movieSource}><button class="viewBtn">View</button></a>
+                        </div>
+                    </div>
+                </div>
+`;
+            recentMovies.appendChild(li)
+        })
+    }
+}
+createRecents();
+
+//trending
+const trendingMovies = document.getElementById("trending");
+
+const fetchTrending = async () => {
+    const response = await fetch("https://mocki.io/v1/fb456028-31c0-44a0-ac2a-d8005d443731")
+    const movies = response.json();
+    return movies;
+};
+
+const createTrending = async () => {
+    const movies = await fetchTrending()
+    if (movies?.length) {
+        movies?.forEach((movie) => {
+            const li = document.createElement("li");
+            li.classList.add("sm")
+            li.innerHTML = `
+                <div class="movies">
+                    <img src=${movie?.movieImg}>
+                    <div class="movieInfo">
+                        <div class="sourceCont">
+                            <div class="sourceText">
+                                <p>Source:</p>
+                            </div>
+                            <div class="source">
+                                <span>${movie?.movieSourceText}</span>
+                                <img src="Icons/images (1).png">
+                            </div>
+                        </div>
+                        <div class="movieTitleCont">
+                            <h4>${movie?.movieTitle}</h4>
+                        </div>
+                        <div class="movieDesc">
+                            <p>${movie?.description}</p>
+                        </div>
+                        <div class="viewLinkCont">
+                            <a href=${movie?.movieSource}><button class="viewBtn">View</button></a>
+                        </div>
+                    </div>
+                </div>
+`;
+            trendingMovies.appendChild(li)
+        })
+    }
+};
+createTrending();
+
+//discovered
+const discoveredMovies = document.getElementById("discover");
+
+const fetchDiscovered = async () => {
+    const response = await fetch("https://mocki.io/v1/7f419f62-d19f-4d47-ad95-d97b2e15d22f")
+    const movies = response.json();
+    return movies;
+};
+
+const createDiscovered = async () => {
+    const movies = await fetchDiscovered()
+    if (movies?.length) {
+        movies?.forEach((movie) => {
+            const li = document.createElement("li");
+            li.classList.add("sm")
+            li.innerHTML = `
+                <div class="movies">
+                    <img src=${movie?.movieImg}>
+                    <div class="movieInfo">
+                        <div class="sourceCont">
+                            <div class="sourceText">
+                                <p>Source:</p>
+                            </div>
+                            <div class="source">
+                                <span>${movie?.movieSourceText}</span>
+                                <img src="Icons/images (1).png">
+                            </div>
+                        </div>
+                        <div class="movieTitleCont">
+                            <h4>${movie?.movieTitle}</h4>
+                        </div>
+                        <div class="movieDesc">
+                            <p>${movie?.description}</p>
+                        </div>
+                        <div class="viewLinkCont">
+                            <a href=${movie?.movieSource}><button class="viewBtn">View</button></a>
+                        </div>
+                    </div>
+                </div>
+`;
+            discoveredMovies.appendChild(li)
+        })
+    }
+};
+createDiscovered();
