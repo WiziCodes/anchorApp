58dbe057e618e97180c9db7fd4fbcf268e50c6df
WiziCodes <wisdomajibade06@gmail.com>
2023-09-10 17:53:08 +1000

    i made changes

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-AOS.init()
+AOS.init();
 
 let slideIndex = 0;
 showSlides();
 function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("slideShow");
-    let spotlight = document.getElementsByClassName("spotlight");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    for (i = 0; i < spotlight.length; i++) {
-        spotlight[i].className = spotlight[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    // spotlight[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
+  let i;
+  let slides = document.getElementsByClassName("slideShow");
+  let spotlight = document.getElementsByClassName("spotlight");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < spotlight.length; i++) {
+    spotlight[i].className = spotlight[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  // spotlight[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
-const toggle = document.querySelector(".toggle")
-const sideBar = document.querySelector(".sideBar")
-const navItems = document.querySelectorAll('.navItems li')
+const toggle = document.querySelector(".toggle");
+const sideBar = document.querySelector(".sideBar");
+const navItems = document.querySelectorAll(".navItems li");
 
-toggle.addEventListener("click", expand)
-
+toggle.addEventListener("click", expand);
 
 function expand() {
-    var state = sideBar.style.width;
-    if (state == "60px") {
-        sideBar.style.width = "130px";
-        navItems.forEach((i) => {
-            i.style.justifyContent = "space-between";
-            i.style.flexDirection = "row";
-            i.style.fontSize = "14px";
-        })
-
-    }
-    else {
-        sideBar.style.width = "60px";
-        navItems.forEach((i) => {
-            i.style.justifyContent = "center";
-            i.style.flexDirection = "column";
-            i.style.fontSize = "10px";
-        })
-    }
+  var state = sideBar.style.width;
+  if (state == "60px") {
+    sideBar.style.width = "130px";
+    navItems.forEach((i) => {
+      i.style.justifyContent = "space-between";
+      i.style.flexDirection = "row";
+      i.style.fontSize = "14px";
+    });
+  } else {
+    sideBar.style.width = "60px";
+    navItems.forEach((i) => {
+      i.style.justifyContent = "center";
+      i.style.flexDirection = "column";
+      i.style.fontSize = "10px";
+    });
+  }
+}
+function closeX() {
+  document.querySelector(".adsCont").style.display = "none";
 }
 // const button = document.getElementById("button")
 // button.addEventListener("click", show)
@@ -67,43 +69,6 @@ function expand() {
 
 // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // var i = 0;
 // var imgs = []
 // var img2 = []
@@ -114,8 +79,6 @@ function expand() {
 // var url2 = "/LandingPage/spidermanBack.jpg"//spiderman
 // var url3 = "/LandingPage/8482387.jpg"//galaxy
 
-
-
 // imgs[0] = `url(${url1})`;//mario
 // img2[0] = `/LandingPage/9048118.jpg`
 
@@ -125,14 +88,12 @@ function expand() {
 // imgs[2] = `url(${url3})`;//galaxy
 // img2[2] = `/LandingPage/9112682.jpg`
 
-
 // titles[0] = '#1 SPOTLIGHT'
 // titles[1] = '#2 SPOTLIGHT'
 // titles[2] = ' #3 SPOTLIGHT'
 // titles2[0] = 'The SUPER MARIO BROS.movie'
 // titles2[1] = 'Spider Man: Across the Spider Verse'
 // titles2[2] = 'Guardians of the Galaxy. volume3'
-
 
 // function slider() {
 //     // document.getElementById("title").innerText = titles[p];
@@ -150,6 +111,3 @@ function expand() {
 // }
 // window.onload = slider;
 // // console.log(imgs.length)
-
-
-
